b84879f52e4d0107c27a1dde0adcff815fc9bcd8
Mikhail Kurenkov <kurenkov1995@mail.ru>
2017-12-11 18:05:49 +0300

    Add protocol class

--- Communication/send_command.py
+++ Communication/send_command.py
@@ -1,33 +1,20 @@
-import serial
+from STMprotocol import STMprotocol
 import sys
-import struct
 
-cmd = int(sys.argv[1])
+protocol = STMprotocol(sys.argv[1])
+
+cmd = int(argv[2])
+parameters = []
+
 if cmd == 1:
-    parameters = bytearray("ECHO")
-elif cmd == 3:
-    ch = int(sys.argv[2])
-    f = float(sys.argv[3])
-    parameters = bytearray([ch]) + bytearray(struct.pack("f", f))
-elif cmd == 4 or cmd == 5:
-    ch = int(sys.argv[2])
-    parameters = bytearray([ch])
+    args = protocol.send_command(cmd, b"ECHO")
+elif cmd == 3 or cmd == 9:
+    args = protocol.send_command(cmd, [int(sys.argv[3]), sys.argv[4]])
+elif cmd == 4 or cmd == 5 or cmd == 10:
+    args = protocol.send_command(cmd, [int(sys.argv[3])])
+elif cmd == 11:
+    args = protocol.send_command(cmd, [int(sys.argv[3]), int(sys.argv[4])])
+else:
+    args = protocol.send_command(cmd, [float(sys.argv[3]), float(sys.argv[4]), float(sys.argv[5])])
 
-msg_len = len(parameters) + 2
-msg = bytearray([0xfa, 0xaf, msg_len, cmd]) + parameters
-crc = sum(msg) % 256
-msg += bytearray([crc])
-
-print(repr(msg))
-ser = serial.Serial("/dev/ttyUSB0", 9600)
-print("send ", repr(msg))
-ser.write(msg)
-
-while(ser.read() != 0xfa):
-    pass
-adr = ser.read()[0]
-answ_len = ser.read()[0]
-answ = bytearray(ser.read(answ_len))
-print("anwer ", repr(answ))
-
-#print(bytearray(ser.read(9)))
+print(args)
